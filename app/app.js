bdb46dd1d852a5300b83e9c16955ce521119d838
Muhāmmad Ranju <muhamadranju@gmail.com>
2023-09-01 15:55:40 +0600

    update app.js

--- app/app.js
+++ app/app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
-const path = require("path");
+const compression = require("compression");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 // Middleware
-app.use([morgan("dev")]);
+app.use([
+  morgan("dev"),
+  compression({
+    level: 6,
+    threshold: 100 * 1000,
+    filter: (req, res) => {
+      if (req.header["x-no-compression"]) {
+        return false;
+      }
+      return compression.filter(req, res);
+    },
+  }),
+]);
 
 // Routes
 app.use(require("../routes/preFix"));
